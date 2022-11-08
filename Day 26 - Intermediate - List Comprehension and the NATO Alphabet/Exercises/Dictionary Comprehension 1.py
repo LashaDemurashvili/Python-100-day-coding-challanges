bf6bcca4f3de0f084ae97d68d65d45b7b9296304
lasha <66213654+LashaDemurashvili@users.noreply.github.com>
2022-11-08 23:16:50 +0400

    added intermediate level

---
+++ Day 26 - Intermediate - List Comprehension and the NATO Alphabet/Exercises/Dictionary Comprehension 1.py
@@ -0,0 +1,8 @@
+#created new dictionary wich nclude word:len(word)
+
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+
+new_list = sentence.split()
+result = {word: len(word) for word in new_list}
+
+print(result)
