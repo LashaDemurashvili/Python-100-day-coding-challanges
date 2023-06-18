5654b97322fe71a3551d3484d425edc8dfc6a49e
lasha <lasha.demurashvili.1@iliauni.edu.ge>
2023-06-19 00:54:27 +0400

    day1 save_00

--- Day 1 Bg/main.py
+++ Day 1 Bg/main.py
@@ -1,6 +1,14 @@
 """
-Program Descriptions
+Day 1 - Beginner - Working with Variables in Python to Manage Data
 
-
-
+Program Descriptions:
+Project - Band Name Generator
 """
+
+print("Welcome to the Band Name Generator.")
+city = input("What's name of the city you grew up in?\n: ")
+pet_name = input("What's your pet's name?\n: ")
+print("Your band name could be: " + city + " " + pet_name)
+
+# using f string method
+print(f"Your band name could be: {city} {pet_name}")
