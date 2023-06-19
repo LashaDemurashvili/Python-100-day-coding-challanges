62de52ef57b3d0441b216a0c47015d8a502f37b7
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-06-20 01:39:31 +0400

    day4 save_03

--- Day 4 Bg/main.py
+++ Day 4 Bg/main.py
@@ -1,6 +1,17 @@
 """
-Program Descriptions
+Day 3 - Beginner - Control Flow and Logical Operators
 
+Program Descriptions:
+
+Condition of the task:
 
 
 """
+
+game_dict = {
+    0: "rock",
+    1: "paper",
+    2: "scissors"
+}
+
+print(game_dict[1])
