d8722e89ddf9e4cb3673fb55965803820ff40249
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-06-20 22:39:17 +0400

    day4 save_07

---
+++ Day 5 Bg/Adding Even Numbers.py
@@ -0,0 +1,36 @@
+"""
+Program Descriptions
+Day 5 - Beginner - Python Loops
+
+Program Descriptions:
+odd even numbers
+"""
+
+
+total_even = 0
+total = 5050
+
+for i in range(2, 101, 2):
+    total_even += i
+    total -= i
+
+print(total_even, "even")
+print(total, "odd")
+
+print("\n")
+print("option 2\n")
+
+even = 0
+odd = 0
+for i in range(101):
+    if i % 2 == 0:
+        even += i
+    else:
+        odd += i
+
+print(even, "even")
+print(odd, "odd")
+
+print((even + odd) == 5050)
+
+
