a9512da9624f86febf68096de8d798f4b6af3625
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-06-21 00:13:46 +0400

    day6 save_01

--- Day 6 Bg/main.py
+++ Day 6 Bg/main.py
@@ -1,6 +1,10 @@
 """
 Program Descriptions
 
+Day 6 - Beginner - Python Functions and karel
 
 
+
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
 """
+
