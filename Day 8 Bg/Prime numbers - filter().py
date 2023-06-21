29a3f1669d5274fce0148dc1bea52d4e5123eb35
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-06-22 00:31:42 +0400

    day8 save_02

--- Day 8 Bg/Prime numbers - filter().py
+++ Day 8 Bg/Prime numbers - filter().py
@@ -2,14 +2,10 @@
 Day 8 - Beginner - Function Parameters & Caesar Cipher
 
 Instructions
-Prime numbers are numbers that can only be cleanly divided by themselves and 1.
-You need to write a function that checks
+Prime numbers are numbers that can only be cleanly divided by themselves and 1.You need to write a function that checks
 whether if the number passed into it is a prime number or not.e.g. 2 is a prime number because it's only divisible
 by 1 and 2.But 4 is not a prime number because you can divide it by 1, 2 or 4.
 """
-
-pr = []
-nt_pr = []
 
 
 def prime_checker(number):
@@ -17,18 +13,16 @@ def prime_checker(number):
     for i in range(2, number):
         if number % i == 0:
             is_prime = False
-    return is_prime
-
-
-print("not prime","|" ,"prime")
-for i in range(1, 100):
-    if prime_checker(i):
-        pr.append(i)
-        print("\t" * 3, i)
+    if is_prime:
+        print(f"{number} It's a prime number.")
     else:
-        nt_pr.append(i)
-        print(i)
+        # print(f"{number} It's not a prime number.")
+        pass
 
-print('\n')
-print(pr)
-print(nt_pr)
+numbers = list(range(1, 101))
+
+a = filter(prime_checker, numbers)
+
+print(list(a))
+
+
