0fadce91e471d38010abef6d5cb8bb3dd5697486
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-06-22 01:18:29 +0400

    day8 save_04

--- Day 8 Bg/Paint Area Calculator.py
+++ Day 8 Bg/Paint Area Calculator.py
@@ -1,4 +1,6 @@
 """
+Day 8 - Beginner - Function Parameters & Caesar Cipher
+
 You are painting a wall. The instructions on the paint can say that 1 can of paint can cover 5 square meters of wall.
 Given a random height and width of wall, calculate how many cans of paint you'll need to buy.
 number of cans = (wall height ✖️ wall width) ÷ coverage per can.
