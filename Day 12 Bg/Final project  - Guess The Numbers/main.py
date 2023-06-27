80cc4e6c345c96da86727eafc943b8c9951b4716
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-06-28 01:41:56 +0400

    day12 save_03

--- Day 12 Bg/Final project  - Guess The Numbers/main.py
+++ Day 12 Bg/Final project  - Guess The Numbers/main.py
@@ -1,4 +1,5 @@
 """
+Day 12 - Beginner - Scope & Number Guessing Game
 Final project  - Guess The Numbers
 
 generate random number from 1 to 100
@@ -14,29 +15,47 @@ from colorama import Fore, Back, Style
 # Initialize colorama
 colorama.init(autoreset=True)
 
+# create lists to calculate final result
+results_list = []
+
+# create dict to calculate final result
+results_dict = {
+    "win": 0,
+    "loss": 0,
+}
+
 
 def guess_number():
+    # generate random number
+    f_num = 1
+    l_num = 100
+    rand_number = random.randint(f_num, l_num)
+
+    # welcome
     print(logo)
     print("Welcome to the Number Guessing Game!")
-    print("I'm thinking of a number between 1 and 100.")
-
-    # generate random number
-    rand_number = random.randint(1, 100)
+    print(f"I'm thinking of a number between {f_num} and {l_num}.")
 
     # test
     print(rand_number)
 
     try:
-        level = input("Choose a difficulty. Type 'easy' or 'hard': ")
-        if level == "easy":
-            attempts = 10
-        else:
+        level = input("Choose a difficulty. Type 'e'/'easy' or 'h'/'hard': ")
+        if level == "hard" or level == "h":
             attempts = 5
+        else:
+            attempts = 10
         print(f"You have {attempts} attempts remaining to guess the number")
 
         while True:
             if attempts == 0:
                 print(f"{Fore.LIGHTRED_EX}You lose !!!")
+
+                # append to list 1 is win 0 is loss
+                results_list.append(0)
+
+                # iterate
+                results_dict["loss"] += 1
                 break
 
             print(f"You have >> {attempts} << attempts")
@@ -44,6 +63,12 @@ def guess_number():
 
             if guess_num == rand_number:
                 print(f"{Fore.LIGHTGREEN_EX} You Win !!!")
+
+                # append to list 1 is win 0 is loss
+                results_list.append(1)
+
+                # iterate
+                results_dict["win"] += 1
                 break
             elif guess_num > rand_number:
                 print("Guess again.")
@@ -57,13 +82,33 @@ def guess_number():
         print(f"{Back.BLACK} {Fore.RED} {e}")
         print(f"{Fore.GREEN} Program RESTARTED")
 
+        # if error occur call again program
         guess_number()
     else:
+        # if code execute without any error
         if input("You want to play again? y/n \n: ") == "y":
             print(f"{Fore.LIGHTCYAN_EX}New Game START")
+
+            # call again program and stat new game
             guess_number()
         else:
             print(f"{Fore.LIGHTBLUE_EX}Program END !")
+
+            # approach 1 using list to calculate final result
+            """
+            w = results.count(1)
+            l = results.count(0)
+
+            r = round(w / (w + l) * 100, 2)
+            print(f"Win ration is: {r}%")
+            """
+
+            # approach 2 using dict to calculate final result
+            win = results_dict["win"]
+            loss = results_dict["loss"]
+
+            res = round(win / (win + loss) * 100, 2)
+            print(f"{Fore.YELLOW}Win ratio is: {res}% ")
 
 
 guess_number()
