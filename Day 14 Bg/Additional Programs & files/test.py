414720aa25bd1585c5ce677c18443746d3dc59d6
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-06-28 23:24:14 +0400

    day14 save_03

--- Day 14 Bg/Additional Programs & files/test.py
+++ Day 14 Bg/Additional Programs & files/test.py
@@ -1,14 +1,10 @@
+
 def check_answer(guess, a_followers, b_followers):
-    """Checks followers against user's guess
-    and returns True if they got it right.
-    Or False if they got it wrong."""
     if a_followers > b_followers:
-        return guess == "a"
+        return guess == "a"  # check if guess == "a"
     else:
         return guess == "b"
 
 
-
-x= check_answer("a", 122, 13)
-
+x = check_answer("b", 12, 111)
 print(x)
