9970adf7dbda154f906ecdac2c038d917eae55dd
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-06-29 01:43:58 +0400

    day15 save_03

--- Day 15 Int/Final Project - Coffee Machine/main.py
+++ Day 15 Int/Final Project - Coffee Machine/main.py
@@ -3,34 +3,15 @@ Day 15 - Intermediate - Local Development Environment Setup & the Coffee Machine
 
 Coffee Machine Program
 """
+import colorama as cl
+from colorama import Fore, Back
 
-# from Resources.data import MENU
+from Resources.data import MENU as menu
 
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
+cl.init(autoreset=True)
+
+start = " Welcome to coffee machine "
+print(Fore.YELLOW + start.center(len(start) * 2, "*").title())
 
 profit = 0
 resources = {
@@ -38,6 +19,24 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+
+
+def prices(menu_dict: dict):
+    print("\n")
+    print(f"{Fore.BLUE}See coffee prices!")
+    for item in menu_dict:
+        coffee_name = item
+        coffee_cost = menu_dict[item]["cost"]
+        print(f"{coffee_name} - Price: ${coffee_cost}")
+
+def report():
+    """print resources"""
+    print("\n")
+    print("Here is current resources report")
+    print(f'Water: {resources["water"]}ml')
+    print(f'Milk: {resources["milk"]}ml')
+    print(f'Coffee: {resources["coffee"]}g')
+    print(f"Money: ${profit}")
 
 
 def is_resource_sufficient(order_ingredients):
@@ -61,6 +60,7 @@ def process_coins():
 
 def is_transaction_successful(money_received, drink_cost):
     """Return True when the payment is accepted, or False if money is insufficient."""
+    """We need profit to be Global variable, for modify"""
     if money_received >= drink_cost:
         change = round(money_received - drink_cost, 2)
         print(f"Here is ${change} in change.")
@@ -79,20 +79,32 @@ def make_coffee(drink_name, order_ingredients):
     print(f"Here is your {drink_name} ☕️. Enjoy!")
 
 
+print("Type 'report' if you want to see resources"
+      "\nType 'off' if you want to off machine")
+
+
+# print prices
+prices(menu_dict=menu)
+
 is_on = True
 
 while is_on:
-    prompt = input("What would you like? (espresso/latte/cappuccino): ")
-    if prompt == "off":
-        is_on = False
-    elif prompt == "report":
-        print(f'Water: {resources["water"]}ml')
-        print(f'Milk: {resources["milk"]}ml')
-        print(f'Coffee: {resources["coffee"]}g')
-        print(f"Money: ${profit}")
-    else:
-        drink = MENU[prompt]
-        if is_resource_sufficient(drink["ingredients"]):
-            payment = process_coins()
-            if is_transaction_successful(payment, drink["cost"]):
-                make_coffee(prompt, drink["ingredients"])
+    try:
+        print("\n")
+        prompt = input("What would you like? (espresso/latte/cappuccino): ")
+        if prompt == "off":
+            print(f"{Fore.RED}Process was ended !")
+            is_on = False
+        elif prompt == "report":
+            report()
+        else:
+            drink = menu[prompt]
+            if is_resource_sufficient(drink["ingredients"]):
+                payment = process_coins()
+                if is_transaction_successful(payment, drink["cost"]):
+                    make_coffee(prompt, drink["ingredients"])
+    except:
+        print(f"{Fore.RED} ERROR occur !!! \n Program restarted")
+        continue
+
+
