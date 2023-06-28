af11d3bdc4867774ef3c2128cf7495ac79d46f20
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-06-28 20:19:42 +0400

    day13 save_02

---
+++ Day 13 Bg/Odd or Even.py
@@ -0,0 +1,13 @@
+"""
+Day 13 - Beginner - Debugging How to Find and Fix Errors in your Code
+
+# Debugging Odd or Even
+"""
+
+number = int(input("Which number do you want to check?"))
+
+# we need double equal operator == to check if is or not
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
--- Day 13 Bg/main.py
+++
@@ -1,4 +0,0 @@
-"""
-Day 13 - Beginner - Debugging How to Find and Fix Errors in your Code
-
-"""
