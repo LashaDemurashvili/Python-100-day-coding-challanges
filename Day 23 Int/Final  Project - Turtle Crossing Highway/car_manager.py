55d1a88259a57419fc9249ebf787b3568ec307ba
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-07-06 01:21:10 +0400

    day23 save_01

---
+++ Day 23 Int/Final  Project - Turtle Crossing Highway/car_manager.py
@@ -0,0 +1,33 @@
+import random
+from turtle import Turtle
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class CarManager:
+
+    def __init__(self):
+        self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
+
+    def create_car(self):
+        random_chance = random.randint(-5, 7)  # car created when random number equal to 7
+        if random_chance == 7:
+            new_car = Turtle('square')
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+
+            random_y_pos = random.randint(-250, 250)
+            x_pos = 300
+            new_car.goto(x_pos, random_y_pos)
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(self.car_speed)
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT - 7 # car speed control
---
+++ Day 23 Int/README.md
@@ -0,0 +1,4 @@
+# Day 23 - Intermediate - The Turtle Crossing Capstone Project
+
+## Turtle Crossing Highway
+
