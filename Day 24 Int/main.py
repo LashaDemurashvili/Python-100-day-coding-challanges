273a714a8c9267d6f3b243e8215b7421cc50ae01
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-09-23 20:29:09 +0400

    update files, from  linux

--- Day 24 Int/main.py
+++ Day 24 Int/main.py
@@ -1,3 +1,4 @@
+"""
 my_dict = {
     'name': 'lasha',
     'surname': 'demurashvili'
@@ -6,4 +7,26 @@ for i, [k, v] in enumerate(my_dict.items(), 1):
     print(f"{i}. {k} - {v}")
 
 
-print(my_dict['name'][0])
+me = 'Hello my name is lasha demurashvili, and I am 25'
+print(me.split())
+
+print('my name'.ljust(10, '/'))
+
+"""
+
+
+# code with nonajanashia
+
+
+def process_data(data):
+    match data:
+        case 1:
+            print(1)
+        case 3:
+            print(3)
+        case 4:
+            print(4)
+
+
+process_data(1)
+process_data(44)
