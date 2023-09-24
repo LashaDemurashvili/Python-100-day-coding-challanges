577e9e240dc1db653d046074577122fe7247cccf
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-09-25 00:32:55 +0400

    update files

--- Day 24 Int/Additional Programs & files/test.py
+++ Day 24 Int/Additional Programs & files/test.py
@@ -1,18 +1,17 @@
-calculation_to_units = 24
-name_of_unit = "hours"
-
-
-def days_to_units(num_of_days):
-    return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
+def days_to_units(num_of_days, conversion_unit):
+    if conversion_unit == "hours":
+        return f"{num_of_days} days are {num_of_days * 24} hours"
+    elif conversion_unit == "minutes":
+        return f"{num_of_days} days are {num_of_days * 24 * 60} minutes"
+    else:
+        return "unsupported unit"
 
 
 def validate_and_execute():
     try:
-        user_input_number = int(num_of_days_element)
-
-        # we want to do conversion only for positive integers
+        user_input_number = int(days_and_unit_dictionary["days"])
         if user_input_number > 0:
-            calculated_value = days_to_units(user_input_number)
+            calculated_value = days_to_units(user_input_number, days_and_unit_dictionary["unit"])
             print(calculated_value)
         elif user_input_number == 0:
             print("you entered a 0, please enter a valid positive number")
@@ -24,7 +23,12 @@ def validate_and_execute():
 
 user_input = ""
 while user_input != "exit":
-    user_input = input("Hey user, enter number of days as a comma separated list and I will convert it to hours!\n")
-    list_of_days = user_input.split(", ")
-    for num_of_days_element in set(list_of_days):
-        validate_and_execute()
+    user_input = input("Hey user, enter number of days and conversion unit!\n")
+    if user_input == 'exit':
+        break
+    days_and_unit = user_input.split(":")
+    print(days_and_unit)
+    days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}
+    print(days_and_unit_dictionary)
+    print(type(days_and_unit_dictionary))
+    validate_and_execute()
