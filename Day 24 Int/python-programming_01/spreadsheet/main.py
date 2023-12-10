ea0d5d2cc7b9fa1691d3d0668d0e994b0a6e0ec5
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-12-10 23:03:20 +0400

    updt

--- Day 24 Int/python-programming_01/spreadsheet/main.py
+++ Day 24 Int/python-programming_01/spreadsheet/main.py
@@ -23,6 +23,7 @@ import openpyxl
 inv_file = openpyxl.load_workbook("inventory.xlsx")
 product_list = inv_file["Sheet1"]
 
+# create empty data structures
 products_per_supplier = {}
 total_value_per_supplier = {}
 products_under_10_inv = {}
@@ -30,41 +31,47 @@ products_under_10_inv = {}
 # how many rows
 # print(product_list.max_row)
 
-
+# loop through entire sheet
 for product_row in range(2, product_list.max_row + 1):
+    # get values for each column
+    supplier_name = product_list.cell(product_row, 4).value
+    inventory = product_list.cell(product_row, 2).value
+    price = product_list.cell(product_row, 3).value
+    product_num = product_list.cell(product_row, 1).value
+
+    # add new value in the file
+    inventory_price = product_list.cell(product_row, 5)
 
     # calculation number of products per supplier
-
-    sup_comp_name = product_list.cell(product_row, 4).value
     """using one line if else"""
-    # products_per_supplier[sup_comp_name] = products_per_supplier[
-    #                                            sup_comp_name] + 1 if sup_comp_name in products_per_supplier else 1
+    # products_per_supplier[supplier_name] = products_per_supplier[
+    #                                            supplier_name] + 1 if supplier_name in products_per_supplier else 1
 
-    if sup_comp_name in products_per_supplier:
-        cur_num_product = products_per_supplier.get(sup_comp_name)
-        products_per_supplier[sup_comp_name] = cur_num_product + 1
+    # calculation number of products per supplier
+    if supplier_name in products_per_supplier:
+        current_num_product = products_per_supplier.get(supplier_name)
+        products_per_supplier[supplier_name] = current_num_product + 1
     else:
-        products_per_supplier[sup_comp_name] = 1
+        products_per_supplier[supplier_name] = 1
 
     # calculation total value of inventory per supplier
-
-    inve = product_list.cell(product_row, 2).value
-    pric = product_list.cell(product_row, 3).value
-
-    if sup_comp_name in total_value_per_supplier:
-        total_value_per_supplier[sup_comp_name] += (inve * pric)
+    if supplier_name in total_value_per_supplier:
+        current_total_value = total_value_per_supplier.get(supplier_name)
+        total_value_per_supplier[supplier_name] = current_total_value + inventory * price
     else:
-        total_value_per_supplier[sup_comp_name] = inve * pric
+        total_value_per_supplier[supplier_name] = inventory * price
 
-    x = product_list.cell(product_row, 1).value
-    y = product_list.cell(product_row, 2).value
+    # logic products with inventory less than 10
+    if inventory < 10:
+        products_under_10_inv[int(product_num)] = int(inventory)
 
-    if y < 10:
-        products_under_10_inv[x] = y
-    else:
-        pass
+    # add value for total inventory price
+    inventory_price.value = inventory * price
 
-
-# print(products_per_supplier)
+print(products_per_supplier)
 print(total_value_per_supplier)
 print(products_under_10_inv)
+
+
+inv_file.save("inventory_with_total_value.xlsx")
+
